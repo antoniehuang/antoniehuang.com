ce6eab77bcb4b77651da1afccd008102382d5e99
antoniehuang <antonieyuchi@gmail.com>
2024-07-13 20:58:21 +0100

    Initial customisation

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        About Me
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I am software engineer with experience building and maintaining backend infrastructures, 
+        colleague facing tools and data pipelines. Demonstrated success working in agile teams delivering products to production. 
+        My interest and expertise lies in utilising cloud infrastructure and applying software architecture best practices to deliver robust and maintainable software`}
       </p>
       <div className="my-8">
         <BlogPosts />
